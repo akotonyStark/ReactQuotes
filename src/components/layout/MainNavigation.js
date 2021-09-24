2161a4872093d51a1d12452a5ab63a8b790d1df2
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-09-24 12:03:42 +0000

    updates

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
 
 function MainNavigation() {
-  return <div></div>
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Great Quotes</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink to='/quotes' activeClassName={classes.active}>
+              All Quotes
+            </NavLink>
+            <NavLink to='/new-quote' activeClassName={classes.active}>
+              Add Quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default MainNavigation
